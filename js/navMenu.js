e6e203b9db5eb870aa1d71e4a01378baf0791739
larryg727 <larrygonzales727@gmail.com>
2017-05-05 02:18:25 -0500

    -added animation effects to about section

--- js/navMenu.js
+++ js/navMenu.js
@@ -3,10 +3,44 @@
  */
 "use strict";
 $(document).ready(function () {
+
+    var section;
+
+    //function to check if variable object in viewport
+    $.fn.inView = function () {
+        //Window Object
+        var win = $(window);
+        //Object to Check
+        var obj;
+        obj = $(section);
+        //the top Scroll Position in the page
+        var scrollPosition = win.scrollTop();
+        //the end of the visible area in the page, starting from the scroll position
+        var visibleArea = win.scrollTop() + win.height();
+        //the end of the object to check
+        var objEndPos = (obj.offset().top + obj.outerHeight());
+        return (visibleArea >= objEndPos && scrollPosition <= objEndPos ? true : false)
+    };
+
+    //only runs scroll animations on screens above tis size
+    if ($(window).width() > 767) {
+        $(window).scroll(function () {
+            section = "#aboutSec";
+            if ($("#aboutSec").inView()) {
+                $("#aboutPic").animate({
+                    right: "0"
+                }, 1000);
+                $("#aboutInfo").slideDown(1500);
+                $("#aboutInfo").css("display", "inline-block");
+            }
+        });
+    }
+
     // initialize slider
     $(".portfolioSlider").slick({
         dots: true,
-        speed: 500,
+        autoplay:true,
+         speed: 500,
         prevArrow: '<button type="button" class="slick-prev">Previous</button>',
         nextArrow: '<button type="button" class="slick-next">Next</button>'
     });
